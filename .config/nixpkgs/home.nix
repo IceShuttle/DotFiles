8d98aa48f2b596410a61551e55f3c33c1be20a13
Shivang <shivang@debian>
2023-03-18 16:56:37 +0530

    added_nix

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "shivang";
+  home.homeDirectory = "/home/shivang";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+  home.packages = [
+    pkgs.nitch
+    pkgs.du-dust
+    pkgs.stylua
+    pkgs.htop-vim
+    pkgs.starship
+  ];
+
+  # Let Home Manager install and manage itself.
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
+
+}
